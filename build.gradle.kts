083bdd8965bc299a70ebb74dcbc933dbe293cb5c
adamz <adam@zethrae.us>
2026-08-03 16:20:21 -0700

    0.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 // The published AAR and the Gradle plugin share the org's coordinated version.
 allprojects {
   group = "sh.swifttui"
-  version = "0.5.0"
+  version = "0.6.0"
 }
 
 // Both publishable modules serve to the GitHub Pages static Maven repo
